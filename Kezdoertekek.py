c5dc8222f337bbb48477acebcab4e6fe51dc1ba6
VargaDoro <varga.dorothea.timea@gmail.com>
2024-11-28 08:56:37 +0100

    haladás

--- Kezdoertekek.py
+++ Kezdoertekek.py
@@ -1,23 +1,26 @@
 import random
 
 class Kezdoertekek:
-    def __init__(self, ugyesseg:int, eletero:int, szerencse:int):
-        self.ugyesseg = ugyesseg
-        self.eletero = eletero
-        self.szerencse = szerencse
+    def __init__(self, nev:str, szint:int):
+        self.nev = nev
+        self.szint = szint
         self.allapot = "Alaphelyzet"
-        ugyesseg:int=0
-        eletero:int=0
-        szerencse:int=0
-        print(ugyesseg)
 
-    def kezdes(self, ):
-        self.allapot = "Kezdés"
-        dobas_ugyi:int=int(random.random()*12+1)
-        dobas_elet:int=int(random.random()*12+1)
-        dobas_elet2:int=int(random.random()*12+1)
-        dobas_szer:int=int(random.random*12+1)
+    def __str__(self):
+        return f"Az érték neve: {self.nev}, A szintje: {self.szint}" 
+
+    def ertekadas():
+        dobas_ugyi:int=int(random.random()*6+1)
+        dobas_elet:int=int(random.random()*6+1)
+        dobas_elet2:int=int(random.random()*6+1)
+        dobas_szer:int=int(random.random()*6+1)
 
         ugyesseg:int=dobas_ugyi+6
-        eletero:int=dobas_elet+12
+        eletero:int=dobas_elet+dobas_elet2+12
         szerencse:int=dobas_szer+6
+        
+        ertekek=["Ügyesség", "Életerő", "Szerencse"]
+        szintszam=[ugyesseg, eletero, szerencse]
+
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import HarcMenete
 import JatekMenete
 import Kezdoertekek
+from Kezdoertekek import Kezdoertekek
 import SzerencseProbaja
+
+kezdo_lista=[]
+kezdo_lista.append(Kezdoertekek("Ügyesség"), )
 
 labirintus=JatekMenete.labirintus_generalasa()
 jatekos=Kezdoertekek.Kezdoertekek(6,6,6)#jatekos generalasa
