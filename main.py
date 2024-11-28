6db5406136c49197947d3ec4f48e022e9626e527
Eszti2222 <nagy.eszter2222@gmail.com>
2024-11-28 09:25:12 +0100

    viki_pull

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ import HarcMenete
 import JatekMenete
 from Kezdoertekek import Jatekos
 from SzerencseProbaja import jo_vagy_balszerencse
-import Kezdoertekek
-from Kezdoertekek import Kezdoertekek
-import SzerencseProbaja
 
-kezdo_lista=[]
-kezdo_lista.append(Kezdoertekek("Ügyesség"), )
 
 labirintus=JatekMenete.labirintus_generalasa()
-jatekos=Kezdoertekek.Kezdoertekek(6,6,6)#jatekos generalasa
+jatekos=Jatekos(6,6,6)#jatekos generalasa
 JatekMenete.jatek(labirintus, jatekos)
