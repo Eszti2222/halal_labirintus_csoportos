30265b4ff6cbe01b64ef471f460425093df5492b
pnrmaja <ponauerszilvi1@gmail.com>
2024-11-28 08:03:17 +0100

    jatek menete hozzáadva

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import HarcMenete
 import JatekMenete
 import Kezdoertekek
 import SzerencseProbaja
+
+labirintus=JatekMenete.labirintus_generalasa()
+jatekos=Kezdoertekek.Kezdoertekek(6,6,6)#jatekos generalasa
+JatekMenete.jatek(labirintus, jatekos)
